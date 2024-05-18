fc76afc904d8c54109884804606e750cf21c78e3
Preetham Venkatesh Arivazhagan <parivazh@rice-01.farmshare.sunet>
2024-05-17 20:49:14 -0700

    Pushing entire project!

---
+++ cva6-renaming/xcelium.d/run.lnx8664.23.03.d/files.ts
@@ -0,0 +1,6 @@
+1715575807 /home/users/parivazh/PA_2/cva6-renaming/core/include/ariane_dm_pkg.sv
+1715575807 /home/users/parivazh/PA_2/cva6-renaming/core/include/cv32a6_imac_sv0_config_pkg.sv
+1715575807 /home/users/parivazh/PA_2/cva6-renaming/core/include/riscv_pkg.sv
+1715575807 /home/users/parivazh/PA_2/cva6-renaming/core/include/ariane_pkg.sv
+1715918309 /home/users/parivazh/PA_2/cva6-renaming/core/renaming_map.sv
+1715575808 /home/users/parivazh/PA_2/cva6-renaming/testbenches/simple_tb.sv
