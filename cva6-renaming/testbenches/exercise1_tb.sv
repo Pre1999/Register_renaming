74220636c101524473721d97528877db6af8c1c8
Preetham Venkatesh Arivazhagan <parivazh@rice-01.farmshare.sunet>
2024-05-18 20:05:47 -0700

    Fixed testbench issue - Assignment Done!

--- cva6-renaming/testbenches/exercise1_tb.sv
+++ cva6-renaming/testbenches/exercise1_tb.sv
@@ -62,7 +62,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 5; // ar5
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I0 commit - replace with I0 physical rd
+        waddr_i = 1; // I0 commit - replace with I0 physical rd
         #20 // Cycle 2
         $display("I1: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -74,7 +74,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 1'bx; // don't care
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I1 commit - replace with I1 physical rd
+        waddr_i = 2; // I1 commit - replace with I1 physical rd
         #20 // Cycle 3
         $display("I2: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -86,7 +86,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 3; // ar3
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I2 commit - replace with I2 physical rd
+        waddr_i = 3; // I2 commit - replace with I2 physical rd
         #20 // Cycle 4
         $display("I3: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -95,7 +95,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.valid = 1'b0; // No new instruction
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I3 commit - replace with I3 physical rd
+        waddr_i = 4; // I3 commit - replace with I3 physical rd
         #20 // Cycle 5
         issue_n.valid = 1'b1; // I4
         issue_n.sbe.rd[PHYS_REG_WIDTH-1:0] = 8; // ar8
@@ -115,7 +115,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 0; // ar0
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I4 commit - replace with I4 physical rd
+        waddr_i = 3; // I4 commit - replace with I4 physical rd
         #20 // Cycle 7
         $display("I5: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -151,7 +151,7 @@ module exercise1_tb import ariane_pkg::*; #(
         issue_n.sbe.rs2[PHYS_REG_WIDTH-1:0] = 0; // ar0
         fetch_entry_ready_i = 1'b1;
         we_gp_i = 1'b1;
-        waddr_i = 0; // I7 commit - replace with I7 physical rd
+        waddr_i = 5; // I7 commit - replace with I7 physical rd
         #20 // Cycle 10
         $display("I8: rd %d rs1 %d rs2 %d", 
                  issue_q.sbe.rd[PHYS_REG_WIDTH-1:0],
@@ -159,7 +159,7 @@ module exercise1_tb import ariane_pkg::*; #(
                  issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0]);
         fetch_entry_ready_i = 1'b0; // No new instruction
         we_gp_i = 1'b1;
-        waddr_i = 0; // I8 commit - replace with I8 physical rd
+        waddr_i = 6; // I8 commit - replace with I8 physical rd
         $finish;
     end
 
